fdfea31613458adf5515bfd68061acb438dea2d2
James <49598946+pj1301@users.noreply.github.com>
2020-03-06 11:06:21 +0000

    adding route controller

---
+++ server/controllers/routeController.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const router = () => {
+  routes.route('/test')
+    .get((req, res) => {
+      res.status(200).send({ message: 'Successfully reached the routes controller'  });
+    });
+
+  return routes;
+}
+
+module.exports = router;
