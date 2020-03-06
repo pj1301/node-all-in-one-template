2cea38de0b0b29a99cf2d9f219b5c93ccaedfebe
James <49598946+pj1301@users.noreply.github.com>
2020-03-06 11:05:21 +0000

    adding routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const debug = require('debug')('index');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const appRoutes = require('./controllers/routeController')();
 
 const port = process.env.PORT || '1234';
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static('public'))
+app.use('/route', appRoutes);
 
 app.listen(port, () => {
   debug(`Server listening on port: ${port}`);
